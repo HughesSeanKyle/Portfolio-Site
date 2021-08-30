4f311dbaa07b48864f019ba0b3d6a5648047d0de
HughesSeanKyle <khughessean@yahoo.com>
2021-08-30 09:16:59 +0200

    Updated id's and classes for about section sliders

--- script.js
+++ script.js
@@ -8,41 +8,41 @@ const textBox = document.getElementById('text-box');
 
 // Dark or Light Images
 function imageMode(color) {
-  image1.src = `img/undraw_Data_report_re_p4so_${color}.svg`;
-  image2.src = `img/undraw_feeling_proud_${color}.svg`;
-  image3.src = `img/undraw_Lost_online_re_upmy_${color}.svg`;
+	image1.src = `img/undraw_Data_report_re_p4so_${color}.svg`;
+	image2.src = `img/undraw_feeling_proud_${color}.svg`;
+	image3.src = `img/undraw_Lost_online_re_upmy_${color}.svg`;
 }
 
 // Dark Mode Styles
 function darkMode() {
-  nav.style.backgroundColor = 'rgba(0 0 0 / 50%)';
-  textBox.style.backgroundColor = 'rgba(255 255 255 / 50%)';
-  toggleIcon.children[0].textContent = 'Dark Mode';
-  toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
-  imageMode('dark');
+	nav.style.backgroundColor = 'rgba(0 0 0 / 50%)';
+	textBox.style.backgroundColor = 'rgba(255 255 255 / 50%)';
+	toggleIcon.children[0].textContent = 'Dark Mode';
+	toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
+	imageMode('dark');
 }
 
 // Light Mode Styles
 function lightMode() {
-  nav.style.backgroundColor = 'rgba(255 255 255 / 50%)';
-  textBox.style.backgroundColor = 'rgba(0 0 0 / 50%)';
-  toggleIcon.children[0].textContent = 'Light Mode';
-  toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
-  imageMode('light');
+	nav.style.backgroundColor = 'rgba(255 255 255 / 50%)';
+	textBox.style.backgroundColor = 'rgba(0 0 0 / 50%)';
+	toggleIcon.children[0].textContent = 'Light Mode';
+	toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+	imageMode('light');
 }
 
 // Switch Theme Dynamically
 function switchTheme(event) {
-  // if true, switch to dark mode, else light mode
-  if (event.target.checked) {
-    document.documentElement.setAttribute('data-theme', 'dark');
-    localStorage.setItem('theme', 'dark');
-    darkMode();
-  } else {
-    document.documentElement.setAttribute('data-theme', 'light');
-    localStorage.setItem('theme', 'light');
-    lightMode();
-  }
+	// if true, switch to dark mode, else light mode
+	if (event.target.checked) {
+		document.documentElement.setAttribute('data-theme', 'dark');
+		localStorage.setItem('theme', 'dark');
+		darkMode();
+	} else {
+		document.documentElement.setAttribute('data-theme', 'light');
+		localStorage.setItem('theme', 'light');
+		lightMode();
+	}
 }
 
 // Event Listener
@@ -51,12 +51,12 @@ toggleSwitch.addEventListener('change', switchTheme);
 // Check Local Storage For Theme
 const currentTheme = localStorage.getItem('theme');
 if (currentTheme) {
-  document.documentElement.setAttribute('data-theme', currentTheme);
+	document.documentElement.setAttribute('data-theme', currentTheme);
 
-  if (currentTheme === 'dark') {
-    toggleSwitch.checked = true;
-    darkMode();
-  }
+	if (currentTheme === 'dark') {
+		toggleSwitch.checked = true;
+		darkMode();
+	}
 }
 
 //////////////////////////////////////////////////////////////////////////
@@ -73,9 +73,9 @@ heading.textContent = '';
 
 // Loop through each character and push it inside heading element as a new span element.
 characters.forEach((char) => {
-  const span = document.createElement('span');
-  span.textContent = char;
-  heading.appendChild(span);
+	const span = document.createElement('span');
+	span.textContent = char;
+	heading.appendChild(span);
 });
 
 // Create charcter count variable and set timer with new function called setChar().
@@ -84,16 +84,33 @@ let timer = setInterval(setChar, 100);
 
 //Stop the interval when all characters are rendered else set character span class to fade.
 function setChar() {
-  // Grab first element of all spans (idx 0)
-  const spanChar = document.querySelectorAll(
-    '.hero-section__headings__h1 > span'
-  )[char];
-  console.log(spanChar);
-  // apply the fade classname
-  spanChar.setAttribute('id', 'fade');
-  //   spanChar.className = 'fade';
-  // Increment char variable
-  char++;
-  // Clear timer when incremented to end of characters array
-  char === characters.length && clearInterval(timer);
+	// Grab first element of all spans (idx 0)
+	const spanChar = document.querySelectorAll(
+		'.hero-section__headings__h1 > span'
+	)[char];
+	console.log(spanChar);
+	// apply the fade classname
+	spanChar.setAttribute('id', 'fade');
+	//   spanChar.className = 'fade';
+	// Increment char variable
+	char++;
+	// Clear timer when incremented to end of characters array
+	char === characters.length && clearInterval(timer);
+}
+
+//////////////////////////////////////////////////////////////////////////
+
+// About section sliders (past, present, future) logic
+
+function aboutTenseSliders() {
+	// Create object to hold images
+	// example (object containing arrays with images for slides)
+	let times = {
+		past: ['past-slide-one', 'past-slide-two', 'past-slide-three'],
+		present: ['present-slide-one', 'present-slide-two', 'present-slide-three'],
+		future: ['future-slide-one', 'future-slide-two', 'future-slide-three'],
+	};
+	// NOTES
+	// 1. Create an array of all btn<tense> button ids
+	// 2. Figure out how to have event listener for all those next/prev buttons
 }
